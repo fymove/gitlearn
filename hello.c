022d87ad85f5d663f40085b671243f4bbf87c82c
fengyang <yfeng880@163.com>
2019-01-08 23:19:51 +0800

    add hello.c

---
+++ hello.c
@@ -0,0 +1,14 @@
+// Last Update:2019-01-08 23:18:34
+/**
+ * @file hello.c
+ * @brief 
+ * @author zengming@jyxtec.com www.jyxtec.com
+ * @version 0.1.00
+ * @date 2019-01-08
+ */
+
+int main(int argc, char * argv[])
+{
+    printf("hello, world!\n")
+}
+
